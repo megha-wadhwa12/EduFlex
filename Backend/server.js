9b016225d244fd4378c28c486c38b3d7844faa03
unknown <sp577152@gmail.com>
2024-03-22 19:03:22 +0530

    Update

--- Backend/server.js
+++ Backend/server.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 const app = express();
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT
+const port = process.env.PORT || 3000
 
 app.use("/api/Users",require("./Routes/UserRoutes"))
 
