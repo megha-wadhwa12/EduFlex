0da15eac7c3e388419c22c177103c93be6822f60
unknown <sp577152@gmail.com>
2024-03-22 17:45:54 +0530

    Backend Initialised

--- Backend/server.js
+++ Backend/server.js
@@ -1,1 +1,14 @@
-console.log('Helloooo')
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config()
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const port = process.env.PORT
+
+app.use("/api/Users",require("./Routes/UserRoutes"))
+
+app.listen(port, () => {
+  console.log(`App is running on PORT: ${port}`);
+});
